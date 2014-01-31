226174924ac7ba03a07059f6bd5f61315a2ae9e1
Andreas Gullberg Larsen <anjdreas@gmail.com>
2014-01-31 18:53:40 +0100

    Add geometry project and test project.
    * Add Rectangle.cs
    * Add RectangleTests.cs

---
+++ Src/UnitsNet.Geometry/Rectangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace UnitsNet.Geometry
+{
+    public struct Rectangle
+    {
+        public readonly Length2d PointA;
+        public readonly Length2d PointB;
+
+        public Rectangle(Length2d pointA, Length2d pointB)
+        {
+            PointA = pointA;
+            PointB = pointB;
+        }
+
+        public Area Area
+        {
+            get
+            {
+                double dxMeters = Math.Abs(PointB.Meters.X - PointA.Meters.X);
+                double dyMeters = Math.Abs(PointB.Meters.Y - PointA.Meters.Y);
+                return new Area(dxMeters * dyMeters);
+            }
+        }
+
+        public Length Width
+        {
+            get
+            {
+                double dxMeters = Math.Abs(PointB.Meters.X - PointA.Meters.X);
+                return Length.FromMeters(dxMeters);
+            }
+        }
+
+        public Length Height
+        {
+            get
+            {
+                double dyMeters = Math.Abs(PointB.Meters.Y - PointA.Meters.Y);
+                return Length.FromMeters(dyMeters);
+            }
+        }
+    }
+}
---
+++ Tests/UnitsNet.Geometry.Tests.net35/RectangleTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace UnitsNet.Geometry.Tests.net35
+{
+    [TestFixture]
+    public class RectangleTests
+    {
+        [Test]
+        public void MeterToLengthUnits()
+        { 
+            var r = new Rectangle(Length2d.FromMeters(1, 2), Length2d.FromMeters(2, 4));
+            Assert.AreEqual(1, r.PointA.Meters.X);
+            Assert.AreEqual(2, r.PointA.Meters.Y);
+            Assert.AreEqual(2, r.PointB.Meters.X);
+            Assert.AreEqual(4, r.PointB.Meters.Y);
+            Assert.AreEqual(1, r.Width.Meters);
+            Assert.AreEqual(2, r.Height.Meters);
+            Assert.AreEqual(2, r.Area.SquareMeters);
+        }
+    }
+}
